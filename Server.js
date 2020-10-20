625521fbfeebb28f56609c4ec4dbe196181cee9c
roniu <roniunnaslahti2@gmail.com>
2020-10-20 19:24:25 +0300

    getData tehty

--- Server.js
+++ Server.js
@@ -1,6 +1,31 @@
-const express = require('express')
+const express = require('express');
 const app = express();
 
-app.listen(3000, () => console.log("kuuntelen"))
+app.listen(3000, () => console.log("kuuntelen"));
+app.use(express.static("public"));
+app.use(express.json({limit: '1mb'}));
 
-app.use(express.static("public"))
+const paikat = [
+  {
+    "paikka": "Helsinki",
+    "arvostelu": "kivaa oli",
+    "longitude": "24.887296",
+    "latitude": "60.2341376000004"
+  },
+  {
+    "paikka": "Levi",
+    "arvostelu": "Ei niin kivaa ollut",
+    "longitude": "24.8082",
+    "latitude": "67.8040"
+  }
+]
+
+app.get('/api/paikat', function(request, response) {
+  response.send(paikat);
+})
+
+app.post('/api/arvostelu', function(request, response){
+  console.log('Käyttäjän Arvostelu');
+  console.log(request.body);
+  response.send(200);
+})
--- public/js/sijainti_ja_kartta.js
+++ public/js/sijainti_ja_kartta.js
@@ -3,29 +3,52 @@ var longitude = 0;
 var olet_tassa;
 var kartta;
 
+async function getData() {
+  const response = await fetch('api/paikat');
+  const data = await reponse.json();
+  console.log(data);
+}
+
 if ("geolocation" in navigator) {
-  console.log("Sijaintitieto saatavilla")
+  console.log("Sijaintitieto saatavilla");
   navigator.geolocation.getCurrentPosition(function(position) {
     console.log(position);
 
     latitude = position.coords.latitude;
     longitude = position.coords.longitude;
 
-    console.log("latitude: " + latitude)
-    console.log("longitude " + longitude)
+    console.log('latitude: ' + latitude);
+    console.log('longitude: ' + longitude);
 
-    document.getElementById("pituusasteet").innerHTML = latitude
-    document.getElementById("leveysasteet").innerHTML = longitude
+    document.getElementById("pituusasteet").innerHTML = latitude;
+    document.getElementById("leveysasteet").innerHTML = longitude;
 
-    kartta= L.map("kartta").setView([latitude, longitude], 13);
-
+    kartta = L.map('kartta').setView([latitude, longitude], 13);
 
     L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-      ttribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>'
+      attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>'
     }).addTo(kartta);
 
-
+    var marker = L.marker([latitude, longitude]).addTo(kartta);
+    getData();
   });
 } else {
-  console.log ("Sijaintitiedot ei saatavilla");
+  console.log("Sijaintitiedot ei saatavilla");
+}
+
+function avaa_paikkatietolomake() {
+  document.getElementById("paikkatietolomake").style.display = "block";
+  document.getElementById("lomake").reset();
+}
+
+function sulje_paikkatietolomake() {
+  document.getElementById("paikkatietolomake").style.display = "none";
+}
+
+function laheta_arvostelu() {
+  var paikka = document.getElementById("paikka").value;
+  var arvostelu = document.getElementById("arvostelu").value;
+
+  console.log("Paikka: " + paikka);
+  console.log("Arvostelu: " + arvostelu);
 }
